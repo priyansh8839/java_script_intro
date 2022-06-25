10b9acd79d761e9606daf4da93093c5412537267
priyansh8839 <105668492+priyansh8839@users.noreply.github.com>
2022-06-25 21:03:49 +0530

    Add files via upload

---
+++ Java Script intro/life.js
@@ -0,0 +1,12 @@
+ let  myname = 'Priyansh Dubey';
+
+ let  myheight = '6.2';
+let  myage = '28'; 
+let  country = 'India';
+
+console.log (myname);
+console.log (myheight);
+console.log (myage);
+console.log (country
+);
+
